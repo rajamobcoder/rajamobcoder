51b252a5df75ceb39998cf5a0090813d616d408b
rajamobcoder <raja.singh@mobcoder.com>
2022-06-16 14:04:04 +0530

    Added CheckBox with databinding

--- app/src/main/java/com/code_rv_mvvm/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/code_rv_mvvm/viewmodel/HomeViewModel.kt
@@ -1,21 +1,28 @@
 package com.code_rv_mvvm.viewmodel
 
+import android.util.Log
+import android.widget.CompoundButton
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.code_rv_mvvm.model.User
 
+
 class HomeViewModel : ViewModel() {
 
     //variable that will listen to user's input
-     var _userNameInput = MutableLiveData<String>()
-     var _userEmailInput = MutableLiveData<String>()
+    var _userNameInput = MutableLiveData<String>()
+    var _userEmailInput = MutableLiveData<String>()
+    var _score = MutableLiveData<String>()
+    var isChecked = MutableLiveData<Boolean>()
 
     private val username: LiveData<String>
         get() = _userNameInput
 
     private val userEmail: LiveData<String>
         get() = _userEmailInput
+
+
 
     fun addUser() {
         val userNames: String? = username.value
